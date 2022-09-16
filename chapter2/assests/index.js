06cee91ebb06f848120b7a0bbd090977e2ccbfa8
Peter McClelland <peterwmcclelland@gmail.com>
2022-09-15 20:07:19 -0500

    quote calculator

--- chapter2/assests/index.js
+++ chapter2/assests/index.js
@@ -1,9 +1,9 @@
-
 function doMath() {
     var people = document.getElementById('people').value;
     var hours = document.getElementById('hours').value;
 
-    var theProduct = parseInt(people) * parseInt(hours) * 5;  
-    document.write(theProduct);
+    var grandTotal = parseInt(people) * parseInt(hours) * 7;  
+    document.getElementById("value").innerHTML = grandTotal;
+
 
 }
