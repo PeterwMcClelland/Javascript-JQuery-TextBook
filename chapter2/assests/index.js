c57a3de8e9de2d288c50076a32f3ad8b6186f980
Peter McClelland <peterwmcclelland@gmail.com>
2022-09-16 14:54:24 -0500

    math update

--- chapter2/assests/index.js
+++ chapter2/assests/index.js
@@ -2,7 +2,7 @@ function doMath() {
     var people = document.getElementById('people').value;
     var hours = document.getElementById('hours').value;
 
-    var grandTotal = parseInt(people) * parseInt(hours) * 7;  
+    var grandTotal = parseInt(people) * parseInt(hours) * 7 + 125;  
     document.getElementById("value").innerHTML = grandTotal;
 
 
